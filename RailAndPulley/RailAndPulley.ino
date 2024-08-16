e63e8192067e037ca880b3a114634478169bb886
jambox <nonya@business.com>
2024-08-15 18:51:27 -0700

    fix: usage of TIMER1 OVF conflicts with the servo library so we are switching to ticking our state machine on TIMER2 OVF

--- RailAndPulley/RailAndPulley.ino
+++ RailAndPulley/RailAndPulley.ino
@@ -6,9 +6,9 @@ volatile int val = 0;
 
 RailAndPulley robot;
 
-ISR(TIMER1_OVF_vect)
+ISR(TIMER2_OVF_vect)
 {
-  TCNT1 = 45535; // Timer Preloading
+  TCNT2 = 99; // Timer Preloading
   // Handle The 10ms Timer Interrupt
   // This triggers the tick function of the robot to check state
   robot.tick();
@@ -16,11 +16,12 @@ ISR(TIMER1_OVF_vect)
  
 void setup()
 {
-  TCCR1A = 0;           // Init Timer1A
-  TCCR1B = 0;           // Init Timer1B
-  TCCR1B |= B00000010;  // Prescaler = 8
-  TCNT1 = 45535;        // Timer Preloading
-  TIMSK1 |= B00000001;  // Enable Timer Overflow Interrupt
+  // Thanks to this lovely little calculator: https://deepbluembedded.com/arduino-timer-calculator-code-generator/
+  TCCR2A = 0;           // Init Timer2A
+  TCCR2B = 0;           // Init Timer2B
+  TCCR2B |= B00000111;  // Prescaler = 1024
+  TCNT2 = 99;        // Timer Preloading
+  TIMSK2 |= B00000001;  // Enable Timer Overflow Interrupt
   pinMode(LED_pin, OUTPUT);
   Serial.begin(9600);
   while (!Serial) {;}
